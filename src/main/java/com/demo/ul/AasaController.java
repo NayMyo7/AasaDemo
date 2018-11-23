b720bacdb97ede2dd54952aa54e2090682a09055
Nay Myo Htet <naymyohtet.hmn@gmail.com>
2018-11-23 14:36:13 +0630

    [initial] apple-app-site-association

---
+++ src/main/java/com/demo/ul/AasaController.java
@@ -0,0 +1,28 @@
+package com.demo.ul;
+
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AasaController {
+	@Autowired
+	private ResourceLoader resourceLoader;
+
+	@GetMapping("/apple-app-site-association")
+	public ResponseEntity<Resource> getAASA() throws IOException {
+		Resource res = resourceLoader.getResource("classpath:apple-app-site-association");
+		return ResponseEntity.ok()
+				.contentLength(res.contentLength())
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(res);
+	}
+
+}
---
+++ src/main/java/com/demo/ul/UniversallinksdemoApplication.java
@@ -0,0 +1,12 @@
+package com.demo.ul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversallinksdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversallinksdemoApplication.class, args);
+	}
+}
